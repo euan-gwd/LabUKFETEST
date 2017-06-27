024d29c6a3503967e1bc719cf53c999b514ad89a
Euan_G <euan@amwic.co.za>
2017-06-27 22:28:25 +0200

    Clean up assets

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,14 @@ var pkg = require('./package.json');
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
+    gulp.src(['node_modules/font-awesome/fonts/**/*'])
+        .pipe(gulp.dest('vendor/fonts'))
+    gulp.src(['node_modules/font-awesome/css/**/*'])
+        .pipe(gulp.dest('vendor/font-awesome'))
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
+    gulp.src(['node_modules/tether/dist/js/*.js'])
+        .pipe(gulp.dest('vendor/tether'))
 })
 
 // Default task
